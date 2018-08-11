8585056b896e8efe3edb51d9e4a33167fffa814c
John Weland <john.weland@gmail.com>
2018-08-10 19:06:19 -0500

    reworked some utils

--- js/app.js
+++ js/app.js
@@ -109,22 +109,94 @@ const file_system = {
  * @namespace
  */
 const uploader = {
-    create: () => {
+    /**
+     * initialize a modal using bootstrap modals
+     */
+    init: () => {
+        let modal = $('<div>', {class: 'modal fade bd-example-modal-lg', tabindex: "-1", role: 'dialog', 'aria-labelledby':'myLargeModalLabel', 'aria-hidden':'true'}).appendTo('body');
+        let dialog = $('<div>', {class: 'modal-dialog modal-dialog-centered modal-lg'}).appendTo(modal);
+        let content = $('<div>', {class: 'modal-content'}).appendTo(dialog);
+        let header = $('<div>', {class: 'modal-header'}).appendTo(content);
+        $('<h5>', {class: 'modal-title', id: '', text: 'Add a file'}).appendTo(header);
+        let button = $('<button>', {type: 'button', class:'close', 'data-dismiss': 'modal', 'aria-label':'Close'}).appendTo(header);
+        $('<span>', {'aria-hidden': 'true', text: `×`}).appendTo(button);
+
+        let body = $('<div>', {class: 'modal-body'}).appendTo(content);
+
+        let footer = $('<div>', {class: 'modal-footer'}).appendTo(content);
+        $('<button>', {type: 'button', class:'btn btn-secondary', 'data-dismiss':'modal', text: 'Close'}).appendTo(footer);
+        $('<button>', {type: 'button', class: 'btn btn-primary', text: 'Add'}).appendTo(footer);
+
+        let form = $('<form>', {class: 'form-horizontal'}).appendTo(body);
+        let fieldset = $('<fieldset>').appendTo(form);
+        $('<legend>', {text: 'File Upload'}).appendTo(fieldset);
+        let form_group = $('<div>', {class: 'form-group'}).appendTo(fieldset);
+        $('<label>', {class: 'control-label', for: 'new_file_name', text: 'Name'}).appendTo(form_group);
+        $('<input>', {class: 'form-control', id: 'new_file_name'}).appendTo(form_group);
+
+        form_group = $('<div>', {class: 'form-group'}).appendTo(fieldset);
+        $('<label>', {class: 'control-label', for: 'new_file_platform', text: 'Platform'}).appendTo(form_group);
+        let select = $('<select>', {class: 'form-control', id: 'new_file_region'}).appendTo(form_group);
+        let platforms = ["NES", "SNES", "GameBoy", "PlayStation", "Genesis/MegaDrive"];
+        $.each(platforms, (key, value) => {
+            $('<option>', {value: value, text: value}).appendTo(select);
+        });
+
+        form_group = $('<div>', {class: 'form-group'}).appendTo(fieldset);
+        $('<label>', {for: 'new_file', class: 'control-label', text: 'File'}).appendTo(form_group);
+        $('<input>', {type: 'file', class: 'form-control-file', id: 'new_file'}).appendTo(form_group);
+
+        form_group = $('<div>', {class: 'form-group'}).appendTo(fieldset);
+        $('<label>', {class: 'control-label', for: 'new_file_region', text: 'Region'}).appendTo(form_group);
+        select = $('<select>', {class: 'form-control', id: 'new_file_region'}).appendTo(form_group);
+        let regions = ["NTSC-U", "NTSC-J", "NTSC-C", "PAL-A", "PAL-B"];
+        $.each(regions, (key, value) => {
+            $('<option>', {value: value, text: value}).appendTo(select);
+        });
+
+        form_group = $('<div>', {class: 'form-group'}).appendTo(fieldset);
+        $('<label>', {class: 'control-label', for: 'new_file_tags', text: 'Region'}).appendTo(form_group);
+        select = $('<select>', {class: 'form-control', id: 'new_file_tags', multiple: true}).appendTo(form_group);
+        let tags = ["popular", "rare", "danger-fav", "translated"];
+        $.each(tags, (key, value) => {
+            $('<option>', {value: value, text: value}).appendTo(select);
+        });
+
+
+
+
     },
-    add: () => {}
+    /**
+     * clear the model data
+     */
+    clear: () => {}
 }
 
 /**
  * @namespace
  */
 const alert = {
-    create: () => {
-    },
-    add: () => {},
-    remove: () => {}
+    /**
+     * create an alert using bootstrap alerts
+     * @param {object} ui Object of data
+     * Object.level ['primary', 'secondary', 'success', 'danger' 'warning', 'info', 'light', 'dark']
+     * Object.heading [optional] "string of text"
+     * Object.message "string of Text"
+     */
+    create: (ui) => {
+        let alert = $('<div>', {class: `alert alert-${ui.level} alert-dismissible fade show`, role:'alert'}).prependTo('body');
+        if (ui.heading) {
+            $('<strong>', {text: `${ui.heading} `}).appendTo(alert);
+        }
+        alert.append(ui.message);
+        let dismiss = $('<button>', {type: 'button', class: 'close', 'data-dismiss': 'alert', 'aria-label': 'close'}).appendTo(alert);
+        $('<span>', {'aria-hidden': 'true', text: `×`}).appendTo(dismiss);
+        $(alert).alert();
+    }
 }
 
 $(document).ready(() => {
     file_system.windowLoad(); 
     dt.windowLoad();
+    uploader.init();
 });
